d8be6736c822b988b724d63a7dc6085b575552c8
Jesper Almstrom <jesper.almstrom@gmail.com>
2022-12-26 12:10:10 +0100

    Change layout and use more fonts. Add readme info

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# tibber-price-arduino
-Print out the Tibber electric price on a TTGO e-paper display
+# Display Tibber price on Arduino E-Paper
+
+Print out the Tibber electric price on a [Lilygo TTGO e-paper display 2.13 inch](http://www.lilygo.cn/prod_view.aspx?TypeId=50061&Id=1393&FId=t3:50061:3) ESP32
+
+![TTGo E-Paper](https://0.rc.xiniu.com/g4/M00/06/1E/CgAG0mEFGAaAIcXpAAI_Z3TUE-A398.jpg)
+
+## Device
+
+Used Arduino IDE with ESP32 Dev Module board settings.
+
+## Development
+
+In the loop the time (from NTP) and electric price (from Tibber API) will be printed once every hour.
+
+### Library
+
+- WiFi to connect to SSID to access internet
+- HTTPClient to talk to the Tibber API (Token needed)
+- ArduinoJson to parse the response from Tibber API
+- GxEPD2 to setup and print to the E-Paper
+- Adafruit GFX [Fonts](https://github.com/adafruit/Adafruit-GFX-Library/tree/master/Fonts) 
+- `time.h` to ask for current time from NTP
--- tibber_price.ino
+++ tibber_price.ino
@@ -2,7 +2,8 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>  // add this library to your project
 #include <GxEPD2_BW.h>
-#include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeSerifBold24pt7b.h>
+#include <Fonts/FreeSerifBold9pt7b.h>
 #include "time.h"
 
 #include "wifi_epaper_settings.h"
@@ -39,15 +40,12 @@ void setup() {
 
   // Init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
 
 }
 
 void loop() {
   HTTPClient http;
   String b = "Bearer ";
-
-  printLocalTime();
 
   http.begin(client, tibberApi); 
   // add necessary headers
@@ -95,12 +93,12 @@ double parseResponse(String response) {
 void showCost(double dCost) {
   Serial.println("showCost");
   display.setRotation(1);
-  display.setFont(&FreeMonoBold12pt7b);
+  display.setFont(&FreeSerifBold24pt7b);
   if (display.epd2.WIDTH < 104) display.setFont(0);
   display.setTextColor(GxEPD_BLACK);
   int16_t tbx, tby; uint16_t tbw, tbh;
-  String sCost = "Elpris ";
-  String dispText = sCost+dCost;
+  String sCost = " kr";
+  String dispText = dCost + sCost;
   display.getTextBounds(dispText, 0, 0, &tbx, &tby, &tbw, &tbh);
   // center bounding box by transposition of origin:
   uint16_t x = ((display.width() - tbw) / 2) - tbx;
@@ -112,42 +110,27 @@ void showCost(double dCost) {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(x, y);
     display.print(dispText);
+    display.setFont(&FreeSerifBold9pt7b);
+    display.setCursor(x + tbw + 2, y);
+    display.print("/kWh");
+    
+    showLocalTime(x+2,20);
   }
   while (display.nextPage());
   Serial.println("showCost done");
 }
 
-void printLocalTime(){
+void showLocalTime(int16_t x, int16_t y) {
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
   Serial.println(&timeinfo, "%Y-%m-%d %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println(&timeinfo, "%Y");
-  Serial.print("Hour: ");
-  Serial.println(&timeinfo, "%H");
-  Serial.print("Hour (12 hour format): ");
-  Serial.println(&timeinfo, "%I");
-  Serial.print("Minute: ");
-  Serial.println(&timeinfo, "%M");
-  Serial.print("Second: ");
-  Serial.println(&timeinfo, "%S");
 
-  Serial.println("Time variables");
-  char timeHour[3];
-  strftime(timeHour,3, "%H", &timeinfo);
-  Serial.println(timeHour);
-  char timeWeekDay[10];
-  strftime(timeWeekDay,10, "%A", &timeinfo);
-  Serial.println(timeWeekDay);
-  Serial.println();
+  char timeNow[20];
+  strftime(timeNow,20, "%Y-%m-%d %H:%M:%S", &timeinfo);
+  display.setFont(&FreeSerifBold9pt7b);
+  display.setCursor(x,y);
+  display.print(timeNow);
 }
